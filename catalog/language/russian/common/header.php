bf89d7e1352677a97d1502a4787dc6b061c9be65
cyber <cyberdelia1987@gmail.com>
2014-09-18 01:51:53 +0300

    Pav theme added

--- catalog/language/russian/common/header.php
+++ catalog/language/russian/common/header.php
@@ -2,11 +2,10 @@
 // Text
 $_['text_home']           = 'Главная';
 $_['text_wishlist']       = 'Закладки (%s)';
-$_['text_shopping_cart']  = 'Корзина покупок';
+$_['text_shopping_cart']  = 'Корзина';
 $_['text_search']         = 'Поиск';
 $_['text_welcome']        = '<a href="%s">Войти</a> или <a href="%s">зарегистрироваться</a>';
 $_['text_logged']         = 'Вы вошли как <a href="%s">%s</a> <b>(</b> <a href="%s">Выйти</a> <b>)</b>';
-$_['text_account']        = 'Постоянный покупатель';
-$_['text_checkout']       = 'Оформление заказа';
+$_['text_account']        = 'Мой счет';
+$_['text_checkout']       = 'Заказ';
 $_['text_page']           = 'страница';
-?>
---
+++ catalog/language/russian/module/pavblog.php
@@ -0,0 +1,63 @@
+<?php
+$_['text_write_by'] = 'Автор:';
+$_['text_published_in'] = 'Опубликовано в: ' ;
+$_['text_created_date'] = 'Дата создания:';
+$_['text_hits'] = 'Просмотров:';
+$_['text_comment_count'] = 'Комментариев:';
+$_['text_readmore'] = "читать далее" ;
+$_['text_leave_a_comment'] = 'Оставить комментарий';
+
+
+ $_['error_captcha'] = 'Капча введена неверно';
+ $_['error_email'] = 'Ошибка в E-Mail';
+ $_['error_comment'] = 'Неверный комментарий';
+ $_['error_user'] = 'Неверное имя пользователя';
+ $_['text_in_related_by_tag'] = 'Похожие блоги по тегам' ;
+/**
+ *
+ */
+ $_['text_children'] = 'Дети' ;
+ $_['text_in_same_category'] = 'То же в категории' ;
+ $_['text_list_comments'] = 'Комментарии' ;
+ $_['text_created'] = 'Создано' ;
+ $_['text_postedby'] = 'Автор' ;
+ $_['text_comment_link'] = 'Ссылка на комментарий' ;
+ $_['entry_name'] = 'Имя' ;
+ $_['entry_email'] = "E-Mail" ;
+ $_['entry_comment'] = 'Комментарий' ;
+ $_['text_submit'] = "Отправить" ;
+ $_['text_tags'] = 'Тэги:';
+
+ $_['text_like_this'] = 'Вам нравится эта:';
+  $_['entry_captcha'] = 'Капча' ;
+
+  $_['filter_blog_header_title'] = 'Фильтр блоги % S' ;
+  $_['blogs_latest_header_title'] = 'Последние блоги';
+
+ /* blogcategory модуля */
+ // Заголовок
+$_['blog_category_heading_title'] = 'Категория блога';
+
+// Подпись под аватаром
+$_['text_latest'] = 'Последние ' ;
+$_['text_mostviewed'] = 'Самые популярные' ;
+$_['text_featured'] = 'Лучшее ' ;
+$_['text_bestseller'] = 'Бестселлер ' ;
+
+/* blogcomment модуля */
+// Заголовок
+$_['blogcomment_heading_title'] = 'Последние комментарии ' ;
+
+$_['text_postedby'] = 'Автор ' ;
+/* bloglatest модуля */
+// Заголовок
+$_['bloglatest_heading_title'] = 'Последние ' ;
+
+// Подпись под аватаром
+$_['text_latest'] = 'Последние Блог ' ;
+$_['text_mostviewed'] = 'Самые популярные ';
+$_['text_featured'] = 'Лучшее ' ;
+$_['text_bestseller'] = 'Бестселлер ' ;
+$_['entry_show_readmore'] = 'Читать дальше ';
+$_['text_readmore'] = "читать дальше" ;
+?>
