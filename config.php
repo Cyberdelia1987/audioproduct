8ae2c8735c5705ac9c685a9fc810dd0d9f580255
cyber <cyberdelia1987@gmail.com>
2014-09-18 00:00:39 +0300

    initial commit

---
+++ config.php
@@ -0,0 +1,27 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://audioproduct.local/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://audioproduct.local/');
+
+// DIR
+define('DIR_APPLICATION', '/home/cyber/projects/audioproduct/catalog/');
+define('DIR_SYSTEM', '/home/cyber/projects/audioproduct/system/');
+define('DIR_DATABASE', '/home/cyber/projects/audioproduct/system/database/');
+define('DIR_LANGUAGE', '/home/cyber/projects/audioproduct/catalog/language/');
+define('DIR_TEMPLATE', '/home/cyber/projects/audioproduct/catalog/view/theme/');
+define('DIR_CONFIG', '/home/cyber/projects/audioproduct/system/config/');
+define('DIR_IMAGE', '/home/cyber/projects/audioproduct/image/');
+define('DIR_CACHE', '/home/cyber/projects/audioproduct/system/cache/');
+define('DIR_DOWNLOAD', '/home/cyber/projects/audioproduct/download/');
+define('DIR_LOGS', '/home/cyber/projects/audioproduct/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'wizardry');
+define('DB_DATABASE', 'audioproduct');
+define('DB_PREFIX', 'oc_');
+?>
